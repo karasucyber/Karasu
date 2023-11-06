1caab53dd87e8447dea1d9800ca6312a60303fcc
karasucyber <aquillae777@gmil.com>
2023-11-06 13:44:34 -0300

    up

--- karasu/app/page.tsx
+++ karasu/app/page.tsx
@@ -1,7 +1,13 @@
+
+'use client'
 import Image from 'next/image'
+import Cinema from '@/Componetes 3D/Cinema'
 
 export default function Home() {
   return (
-   <></>
+   <>
+   <Cinema></Cinema>
+   
+   </>
   )
 }
