d90a7c436ebd6a4bd8622a5a0433bbf7e8c39d37
karasucyber <aquillae777@gmil.com>
2023-11-08 23:30:10 -0300

    up

--- karasu/Componetes 3D/Cinema.tsx
+++ karasu/Componetes 3D/Cinema.tsx
@@ -5,14 +5,13 @@ import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
 import "./styles.css"
 export default function Cinema() {
   return (
-    <Canvas concurrent gl={{ alpha: false }} pixelRatio={[1, 1.5]} camera={{ position: [0, 3, 100], fov: 15 }}>
+    <Canvas  gl={{ alpha: false }} camera={{ position: [0, 3, 100], fov: 15 }}>
       <color attach="background" args={['black']} />
       <fog attach="fog" args={['black', 15, 20]} />
       <Suspense fallback={null}>
         <group position={[0, -1, 0]}>
           <Samurai rotation={[0, Math.PI - 0.70, 0]} position={[-1, 0, 2]} scale={[1, 1, 1]} />
           <VideoText position={[0, 1.3, -2]} />
-          <Ground />
         </group>
         <ambientLight intensity={0.5} />
         <spotLight position={[12, 10, 0]} intensity={0.3} />
@@ -23,12 +22,9 @@ export default function Cinema() {
   )
 }
 
-
-
-
 function Samurai(props:any) {
   const colotTexture = useTexture('Robe2_normal.jpeg')
-  const { scene } = useGLTF('/scene.gltf')
+  const { scene } = useGLTF('/Samurai.gltf')
   return <primitive object={scene} map={colotTexture} {...props} />
   
 }
@@ -46,14 +42,14 @@ function VideoText(props: any) {
   )
 }
 
-function Ground() {
+/*function Ground() {
   const [floor, normal] = useTexture(['/SurfaceImperfections003_1K_var1.jpg', '/SurfaceImperfections003_1K_Normal.jpg'])
   return (
     <Reflector blur={[400, 100]} resolution={512} args={[10, 10]} mirror={0.5} mixBlur={6} mixStrength={1.5} rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
       {(Material, props) => <Material color="#a0a0a0" metalness={0.4} roughnessMap={floor} normalMap={normal} normalScale={[2, 2]} {...props} />}
     </Reflector>
   )
-}
+}*/
 
 function Intro() {
   const [vec] = useState(() => new THREE.Vector3())
