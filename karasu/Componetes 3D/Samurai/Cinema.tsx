3e0da219a8191be7d3e84362c3f7e0aed9869495
karasu_corp <aquillae777@gmail.com>
2023-11-20 19:15:49 -0300

    refatoração

--- karasu/Componetes 3D/Samurai/Cinema.tsx
+++ karasu/Componetes 3D/Samurai/Cinema.tsx
@@ -1,38 +1,20 @@
-import * as THREE from 'three'
-import React, { Suspense, useEffect, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
-import "./styles.css"
-export default function Cinema() {
-  return (
-    <Canvas  gl={{ alpha: false }} camera={{ position: [0, 3, 100], fov: 15 }}>
-      <color attach="background" args={['black']} />
-      <fog attach="fog" args={['black', 15, 20]} />
-      <Suspense fallback={null}>
-        <group position={[0, -1, 0]}>
-          <Samurai rotation={[0, Math.PI - 0.70, 0]} position={[-1, 0, 2]} scale={[1, 1, 1]} />
-          <VideoText position={[0, 1.3, -2]} />
-          <Ground/>
-        </group>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[12, 10, 0]} intensity={0.3} />
-        <directionalLight position={[50, 0, -40]} intensity={0.7} />
-        <Intro />
-      </Suspense>
-    </Canvas>
-  )
-}
+import * as THREE from 'three';
+import React, { Suspense, useEffect, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei';
+import './styles.css';
 
-function Samurai(props:any) {
-  const colotTexture = useTexture('Robe2_normal.jpeg')
-  const { scene } = useGLTF('/Samurai.gltf')
-  return <primitive object={scene} map={colotTexture} {...props} />
+const Samurai = React.memo(function Samurai(props: any) {
+  const colorTexture = useTexture('Robe2_normal.jpeg');
+  const { scene } = useGLTF('/Samurai.gltf');
+  return <primitive object={scene} map={colorTexture} {...props} />;
+});
+
+const VideoText = React.memo(function VideoText(props: any) {
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/intro.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }));
   
-}
+  useEffect(() => void video.play(), [video]);
 
-function VideoText(props: any) {
-  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/intro.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }))
-  useEffect(() => void video.play(), [video])
   return (
     <Text font="/Inter-Bold.woff" fontSize={2} letterSpacing={-0.06} {...props}>
       Karasu
@@ -40,11 +22,15 @@ function VideoText(props: any) {
         <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
       </meshBasicMaterial>
     </Text>
-  )
-}
+  );
+});
 
-function Ground() {
+const Ground = React.memo(function Ground() {
   const [floor, normal] = useTexture(['/SurfaceImperfections003_1K_var1.jpg', '/SurfaceImperfections003_1K_Normal.jpg']);
+
+  useEffect(() => {
+    // Carregue texturas aqui de forma assíncrona, se necessário
+  }, []); // Executado apenas uma vez após a montagem do componente
 
   return (
     <Reflector
@@ -68,13 +54,35 @@ function Ground() {
       )}
     </Reflector>
   );
-}
+});
 
+const Intro = React.memo(function Intro() {
+  const [vec] = useState(() => new THREE.Vector3());
 
-function Intro() {
-  const [vec] = useState(() => new THREE.Vector3())
-  return useFrame((state) => {
-    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
-    state.camera.lookAt(0, 0, 0)
-  })
+  return useFrame(
+    (state) => {
+      state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05);
+      state.camera.lookAt(0, 0, 0);
+    },
+  );
+});
+
+export default function Cinema() {
+  return (
+    <Canvas gl={{ alpha: false }} camera={{ position: [0, 3, 100], fov: 15 }}>
+      <color attach="background" args={['black']} />
+      <fog attach="fog" args={['black', 15, 20]} />
+      <Suspense fallback={null}>
+        <group position={[0, -1, 0]}>
+          <Samurai rotation={[0, Math.PI - 0.70, 0]} position={[-1, 0, 2]} scale={[1, 1, 1]} />
+          <VideoText position={[0, 1.3, -2]} />
+          <Ground />
+        </group>
+        <ambientLight intensity={0.5} />
+        <spotLight position={[12, 10, 0]} intensity={0.3} />
+        <directionalLight position={[50, 0, -40]} intensity={0.7} />
+        <Intro />
+      </Suspense>
+    </Canvas>
+  );
 }
