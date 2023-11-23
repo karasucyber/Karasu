6447b8be103d4663a47268badb785f40aea8a543
karasu_corp <aquillae777@gmail.com>
2023-11-22 23:44:32 -0300

    up

--- karasu/app/page.tsx
+++ karasu/app/page.tsx
@@ -1,8 +1,6 @@
-
 'use client'
-import Cinema from '@/Componetes 3D/Samurai/Cinema'
-import Footer from '@/Componetes 3D/Footer/Footer'
-import Globlo from '@/Componetes 3D/block/Box'
+import Cinema from '@/Componetes 3D/Samurai/Cinema';
+import Footer from '@/Componetes 3D/Footer/Footer';
 export default function Home() {
   return (
    <>
