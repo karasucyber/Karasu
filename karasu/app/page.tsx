2c3477b787ae8024aea9580f14a8d28066a69c9b
karasu_corp <aquillae777@gmail.com>
2024-02-20 00:05:27 -0300

    up

--- karasu/app/page.tsx
+++ karasu/app/page.tsx
@@ -1,28 +1,11 @@
 'use client'
 import Cinema from '@/Componetes 3D/Samurai/Cinema';
 import Footer from '@/Componetes 3D/Footer/Footer';
-import styled from 'styled-components';
-
-const Container = styled.div({
-  width:"100%",
-  height: "100%",
-})
-
-const Container1 = styled.body({
-  padding: "0px",
-  margin: "0px",
-  top: "0px",
-
-})
 export default function Home() {
   return (
-   
-   <> 
-    <Cinema/>
+   <>
+   <Cinema/>
    <Footer/>
-    
    </>
-   
- 
   )
 }
