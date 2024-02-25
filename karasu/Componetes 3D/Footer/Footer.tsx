15136b1584f693c1418b40b89014a9a9f31af0d2
karasu_corp <aquillae777@gmail.com>
2024-02-24 23:54:49 -0300

    teste

--- karasu/Componetes 3D/Footer/Footer.tsx
+++ karasu/Componetes 3D/Footer/Footer.tsx
@@ -8,93 +8,7 @@ import { AiFillGithub } from "react-icons/ai";
 import { FaHeadSideVirus } from "react-icons/fa";
 import { MdWork } from "react-icons/md";
 
-const Container = styled.div`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  display: flex;
-  padding: 20px;
-  justify-content: space-between;
-  align-items: flex-end;
-  flex-direction: row;
-  z-index: 9999;
-  @media (max-width: 768px) {
-    flex-direction: column;
-    align-items: center;
-    text-align: center;
-  }
-`;
 
-const ContainerLeft = styled.div`
-  color: white;
-  padding: 10px;
-  width: 100%;
-  background-color: rgba(0, 0, 0, 0.80); 
-  box-shadow: 5px 5px 6px rgba(255, 255, 255, 1);
-  font-size: 20px;
-  margin-right: 30px; // Adicione esta linha para espaçamento à direita
-
-  @media (max-width: 768px) {
-      width: 100%;
-      margin: 10px;
-
-  }
-`;
-
-const ContainerRight = styled.div`
-  color: white;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: 100%;
-  @media (max-width: 768px) {
-    color: white;
-  display: flex;
-  justify-content: center;
-  flex-direction: row; 
-  width: 100%;
-  }
-`;
-
-const ContainerIcons = styled.button`
-  padding: 10px;
-  width: 100%;
-  font-size: 24px;
-  background: transparent;
-  color: white;
-  border: none;
-  @media (max-width: 768px) {
-    display: flex;
-    flex-direction: row; 
-    padding: 10px;
-  }
-`;
-
-const ClickableIcon = styled.a`
-  color: white;
-  text-decoration: none;
-  &:hover {
-    color: #cccccc;
-  }
-`;
-
-const ClickableIcon1 = styled.a`
-  color: white;
-  text-decoration: none;
-  &:hover {
-    color: #cccccc;
-  }
-`;
-
-const Paragrafo = styled.div`
-  font-size: 15px;
-  padding: 10px;
-`;
-
-const ContainerTitulo = styled.div`
-  padding: 10px;
-`;
 
 /*<ContainerLeft>
   <ContainerTitulo><ClickableIcon1>Dev <FaHeadSideVirus />
@@ -102,21 +16,119 @@ const ContainerTitulo = styled.div`
   <Paragrafo>Você Dev que deseja se aprofuncar no mundo linux e rust </Paragrafo>
   </ContainerLeft>
 */
+const Container = styled.div({
+  position: "fixed",
+  bottom: 0,
+  left: 0,
+  width: "100%",
+  display: "flex",
+  padding: "20px",
+  justifyContent: "space-between",
+  alignItems: "flex-end",
+  flexDirection: "row",
+  zIndex: 9999,
+  "@media (max-width: 768px)": {
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+});
+
+const ContainerLeft = styled.div({
+  color: "white",
+  padding: "10px",
+  width: "100%",
+  backgroundColor: "rgba(0, 0, 0, 0.80)",
+  boxShadow: "5px 5px 6px rgba(255, 255, 255, 1)",
+  fontSize: "20px",
+  marginRight: "30px",
+  "@media (max-width: 768px)": {
+    width: "100%",
+    margin: "10px",
+  },
+});
+
+const ContainerRight = styled.div({
+  color: "white",
+  display: "flex",
+  justifyContent: "center",
+  flexDirection: "column",
+  width: "100%",
+  "@media (max-width: 768px)": {
+    color: "white",
+    display: "flex",
+    justifyContent: "center",
+    flexDirection: "row",
+    width: "100%",
+  },
+});
+
+const ContainerIcons = styled.button({
+  padding: "10px",
+  width: "100%",
+  fontSize: "24px",
+  background: "transparent",
+  color: "white",
+  border: "none",
+  "@media (max-width: 768px)": {
+    display: "flex",
+    flexDirection: "row",
+    padding: "10px",
+  },
+});
+
+const ClickableIcon = styled.a({
+  color: "white",
+  textDecoration: "none",
+  "&:hover": {
+    color: "#cccccc",
+  },
+});
+
+const ClickableIcon1 = styled.a({
+  color: "white",
+  textDecoration: "none",
+  "&:hover": {
+    color: "#cccccc",
+  },
+});
+
+const Paragrafo = styled.div({
+  fontSize: "15px",
+  padding: "10px",
+});
+
+const ContainerTitulo = styled.div({
+  padding: "10px",
+});
 
 const Footer = () => {
   return (
     <>
       <Container>
         <ContainerLeft>
-        <ContainerTitulo><ClickableIcon1 href="https://karasucorp.com/">Clientes <AiFillSignal />
-        </ClickableIcon1></ContainerTitulo>
-          <Paragrafo> Seja muito bem-vindo! Ao prosseguir, você será direcionado para o nosso site, onde encontrará informações detalhadas sobre nossos serviços, projetos e expertise.</Paragrafo>
+          <ContainerTitulo>
+            <ClickableIcon1 href="https://karasucorp.com/">
+              Clientes <FaHeadSideVirus />
+            </ClickableIcon1>
+          </ContainerTitulo>
+          <Paragrafo>
+            Seja muito bem-vindo! Ao prosseguir, você será direcionado para o
+            nosso site, onde encontrará informações detalhadas sobre nossos
+            serviços, projetos e expertise.
+          </Paragrafo>
         </ContainerLeft>
 
         <ContainerLeft>
-        <ContainerTitulo><ClickableIcon1 href="/Recrutadores">Recrutores <MdWork />
-        </ClickableIcon1></ContainerTitulo>
-        <Paragrafo>Apresento orgulhosamente minhas certificações e meu portfólio, evidenciando minha dedicação à excelência no mundo tecnológico. </Paragrafo>
+          <ContainerTitulo>
+            <ClickableIcon1 href="/Recrutadores">
+              Recrutores <MdWork />
+            </ClickableIcon1>
+          </ContainerTitulo>
+          <Paragrafo>
+            Apresento orgulhosamente minhas certificações e meu portfólio,
+            evidenciando minha dedicação à excelência no mundo tecnológico.
+          </Paragrafo>
         </ContainerLeft>
 
         <ContainerRight>
@@ -127,7 +139,9 @@ const Footer = () => {
           </ContainerIcons>
 
           <ContainerIcons>
-            <ClickableIcon href="https://api.whatsapp.com/send?phone=+55+5511919216813&text=Ol%C3%A1%2C+estou+vindo+porqu%C3%AA+o+corvo+disse....">
+            <ClickableIcon
+              href="https://api.whatsapp.com/send?phone=+55+5511919216813&text=Ol%C3%A1%2C+estou+vindo+porqu%C3%AA+o+corvo+disse...."
+            >
               <FaWhatsapp />
             </ClickableIcon>
           </ContainerIcons>
@@ -140,14 +154,13 @@ const Footer = () => {
 
           <ContainerIcons>
             <ClickableIcon href="https://github.com/karasucyber/Aquillae777">
-            <AiFillGithub />
+              <AiFillGithub />
             </ClickableIcon>
           </ContainerIcons>
         </ContainerRight>
-
       </Container>
     </>
   );
-}
+};
 
 export default Footer;
