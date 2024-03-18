549c6f4c517405105d3e3dc482c37bb1310a222c
karasu_corp <aquillae777@gmail.com>
2024-03-18 14:05:42 -0300

    up

--- karasu/app/Recrutadores/page.tsx
+++ karasu/app/Recrutadores/page.tsx
@@ -1,29 +1,39 @@
 'use client'
 import React from "react";
-import styled from "styled-components";
+import "./styles.css"
 
-const Container = styled.div({
-    display: "flex",
-    justifyContent: "center",
-    width: "100%",
-    height:"100%",
-})
+export default function Recrutadores() {
+    return (
+        <>
+            <div className="Container">
+            <h1> Meus Projetos</h1>
 
-const ContainerText = styled.div({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-    height: "100%",
-})
+                <div className="Box">
+                    <p className="">  Muito prazer sou conhecido como Karasu(Corvo em japonês) eu desenvolvedor á dois anos atuando principalmente em desenvolvimento web,
+                        sou especislista em desenvolvimento de sites com objetos 3D trazendo inovação e credibilidade para seu site, á baixo você pode ver sites feitos por mim, entre outros Projetos
+                        que o foco era a funcionalidade e não o visual. Aqui temos, Projetos WebSite, mobile, microServiços entre outros.
+                    </p>
+                </div>
+                <h1> WebSites</h1>
+                <div className="Box1">
 
-export default function Recrutadores(){
-    return(
-    <Container> 
-        <h1> Pagina em desenvolvimento</h1>
+                 <div className="Card"> 
+                 <h2> Home Fight</h2>
+                    <div>
+                         <div className="ContainerImg">
+                            <img className="Img" src="Captura de tela de 2023-11-06 21-05-05.png"/>
+                         </div>
+                      <h2> Descrição</h2>
+                      <p> aqui .....</p>
+                    </div>
+                    </div>
 
 
-    </Container>
-    
-)
+
+
+                </div>
+            </div>
+        </>
+
+    )
 }
