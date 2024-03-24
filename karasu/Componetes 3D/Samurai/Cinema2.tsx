e8a2bfd6fa7ceb726fab3e91b9ad8d08bfcc02c8
karasu_corp <aquillae777@gmail.com>
2024-03-24 15:55:25 -0300

    Up

---
+++ karasu/Componetes 3D/Samurai/Cinema2.tsx
@@ -0,0 +1,74 @@
+import * as THREE from 'three'
+import React, { Suspense, useEffect, useState } from 'react'
+import { Canvas, useFrame, useThree } from '@react-three/fiber'
+import { Reflector, Text, useTexture, useGLTF } from '@react-three/drei'
+import "./styles.css"
+// Remove the following import lines
+// import { Text, useThree } from 'react-three-fiber';
+// import { videoTexture, sRGBEncoding, meshBasicMaterial } from 'three';
+export default function Cinema() {
+  return (
+    <Canvas  gl={{ alpha: false }} camera={{ position: [0, 3, 100], fov: 15 }}>
+      <color attach="background" args={['black']} />
+      <fog attach="fog" args={['black', 15, 25]} />
+      <Suspense fallback={null}>
+        <group position={[0, -1, 0]}>
+          <VideoText position={[0, 1.3, -2]} />
+          <Ground/>
+        </group>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[60, 100, -40]} intensity={0.4} />
+        <Intro />
+      </Suspense>
+    </Canvas>)}
+
+
+function VideoText(props: any) {
+  const { size } = useThree(); 
+  const responsiveFontSize = size.width > 780 ? 3 : 0.50; 
+  const [video] = useState(() => Object.assign(document.createElement('video'), { src: '/intro.mp4', crossOrigin: 'Anonymous', loop: true, muted: true }));
+
+  useEffect(() => {
+    void video.play();
+  }, [video]);
+  
+  return (
+    <Text font="/Inter-Bold.woff" fontSize={responsiveFontSize} letterSpacing={-0.10} {...props}>
+      Projetos
+      <meshBasicMaterial toneMapped={false}>
+        <videoTexture attach="map" args={[video]} encoding={THREE.sRGBEncoding} />
+      </meshBasicMaterial>
+    </Text>);}
+    
+function Ground() {
+  const [floor, normal] = useTexture(['/LowSet1_baseColor.png', '/LowSet1_baseColor.png']);
+  return (
+    <Reflector
+      blur={[400, 100]}
+      resolution={512}
+      args={[10, 10]}
+      mirror={0.5}
+      mixBlur={6}
+      mixStrength={1.5}
+      rotation={[-Math.PI / 2, 0, Math.PI / 2]}>
+        
+      {(Material: React.ElementType, props: any) => (
+        <Material
+          color="black"
+          metalness={0.4}
+          roughnessMap={floor}
+          normalMap={normal}
+          normalScale={[2, 2]}
+          {...props} 
+        />
+      )}
+    </Reflector>
+  );
+}
+function Intro() {
+  const [vec] = useState(() => new THREE.Vector3())
+  return useFrame((state) => {
+    state.camera.position.lerp(vec.set(state.mouse.x * 5, 3 + state.mouse.y * 2, 14), 0.05)
+    state.camera.lookAt(0, 0, 0)
+  })
+}
--- karasu/app/Recrutadores/page.tsx
+++ karasu/app/Recrutadores/page.tsx
@@ -8,48 +8,128 @@ import './styles.css';
 import { AiOutlineCaretLeft } from "react-icons/ai";
 
 import { EffectCards } from 'swiper/modules';
+import Cinema2 from "@/Componetes 3D/Samurai/Cinema2";
 
 
 
 export default function Recrutadores() {
     return (
         <>
+            <div style={{ width: "100%", height:" 400px" }} >
+                  <Cinema2/>
+            </div>
             <div className="Container">
-                <h1 className="Titulo"> Portifolio </h1>
-
                 <div className="ContainerCard">
                     <h1 className="Titulo"> WebSites </h1>
                     <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide>
+                            <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://www.agencier.com.br/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://www.semlimitesed.tech/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://clientenaopagante.vercel.app/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://home-fight.vercel.app/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>  
+                        <SwiperSlide>
+                            <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://my-house-777-2nbp.vercel.app/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>  
                     </Swiper>
                 </div>
 
                 <div className="ContainerCard">
                     <h1 className="Titulo"> Back-End </h1>
                     <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
-                        <SwiperSlide></SwiperSlide>
+                        <SwiperSlide>
+                        <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://in8teste1.vercel.app/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://bibliotecanucleo.vercel.app/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://bibliotecanucleo.vercel.app/">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://github.com/karasucyber/Cadastro_J">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                        <div className="slide-content">
+                                <img src="Captura de tela de 2023-11-06 21-05-05.png" alt="Imagem 1" />
+                                <div className="button-container">
+                                    <button  className="button-below"> 
+                                    <a href="https://tarefa-2-estacio.vercel.app/procedimento%201/index2.html">Projeto</a></button>
+                                </div>
+                            </div>
+                        </SwiperSlide>
                     </Swiper>
                 </div>
                 <div className="Seta">
-                    <AiOutlineCaretLeft style={{ height: "100px", width: "200px", color: "white" }} />
+                    <button style={{ height: "100px", width: "200px", backgroundColor: "transparent", border: "none", cursor: "pointer" }}>
+                        <a href="/" style={{ color: "white", textDecoration: "none" }}>
+                            <AiOutlineCaretLeft style={{ height: "90%", width: "100%" }} />
+                        </a>
+                    </button>
                 </div>
+
             </div>
+
         </>
     )
 }
