835744802d58094611549a610881d2b661a5d33e
karasucyber <aquillae777@gmail.com>
2025-05-17 22:25:51 -0300

    teste

---
+++ karasu_v2/src/app/Components/Navbar/Navbar.componet.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'',
+    templateUrl:'./Navbar.component.html',
+    styleUrls: ['./Navbar.component.css'],
+})
+
+export class helloComponent{
+
+    
+}
